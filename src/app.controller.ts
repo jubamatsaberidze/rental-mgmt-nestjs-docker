825c85c4cd39a684f9f75dfd81392f1ac99302a5
jubamatsaberidze <j.matsaberidze.websmart@gmail.com>
2022-12-07 13:20:04 +0400

    configured and added RMQ listener

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Logger } from '@nestjs/common';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 import { AppService } from './app.service';
+
+interface CreateCarDto {
+  name: string;
+  license_plate: string;
+}
+
+const logger = new Logger('FileUploadApplication');
 
 @Controller()
 export class AppController {
@@ -7,5 +20,20 @@ export class AppController {
   @Get('/cars')
   getCars(): Promise<any> {
     return this.appService.getCars();
+  }
+
+  @Post('/cars')
+  addCar(@Body() createCarDto: CreateCarDto): Promise<string> {
+    return this.appService.addCar(createCarDto);
+  }
+
+  @MessagePattern('append_cars')
+  public async execute(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    logger.verbose('data => ', data);
+    await this.addCar(data);
+    channel.ack(originalMessage);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,26 @@ import { DatabaseService } from './database/database.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
   async getCars(): Promise<any> {
-    const res = new DatabaseService();
-    const req = res.executeQuery(`SELECT * FROM cars`);
-    return req;
+    const res = this.databaseService.executeQuery(`SELECT * FROM cars`);
+    return res;
+  }
+
+  async addCar(createCarDto): Promise<string> {
+    const { name, license_plate } = createCarDto;
+    if (!name || !license_plate) {
+      return 'Invalid values.';
+    }
+    const res = await this.databaseService.executeQuery(
+      `INSERT INTO cars("name", "license_plate")
+       VALUES('${name}', '${license_plate}');
+      `,
+    );
+
+    return res
+      ? 'Successfully added new car.'
+      : 'Something went wrong...\nTry again later';
   }
 }
